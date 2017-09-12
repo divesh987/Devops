23caccf87ba03ac9eb88d8a581f0e8b10bb4f7b9
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-12 14:25:59 +0100

    addd sudo

--- environment/provision.sh
+++ environment/provision.sh
@@ -4,4 +4,4 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-npm install pm2 -g
+sudo npm install pm2 -g
