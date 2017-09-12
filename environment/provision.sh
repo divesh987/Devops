aaf58dfd28b3286b5e65e1160df121d454530f61
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-12 15:06:03 +0100

    reversed proxy from port 80 to port 3000

--- environment/provision.sh
+++ environment/provision.sh
@@ -2,6 +2,13 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
+sudo rm /etc/nginx/sites-available/default 
+sudo cp app/environment/nginx.default /etc/nginx/sites-available/default
+sudo service nginx restart
+
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install pm2 -g
+
+
+
