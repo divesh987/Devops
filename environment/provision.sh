a5ef5411697bf22a6262d230cefbba4be2fada9c
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-12 15:23:47 +0100

    updated provision

--- environment/provision.sh
+++ environment/provision.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 sudo rm /etc/nginx/sites-available/default 
-sudo cp app/environment/nginx.default /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/app/environment/nginx.default /etc/nginx/sites-available/default
 sudo service nginx restart
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
